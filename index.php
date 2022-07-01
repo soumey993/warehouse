c06d53359e6b8748ad182d9610257f2b3390fc69
tokaya20 <tokayamoubachar@gmail.com>
2022-07-01 17:05:05 +0100

    Index update

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Gestion de Magasins et Boutiques</title>
+  <title>Gestion </title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
